e4975dde78c0f252c57b9a915a37ff4c7d9005e3
letrananhvu <letrananhvu>
2020-07-03 14:30:19 +0300

    part4 4.5 - done

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -4,8 +4,20 @@ const dummy = (blogs) => {
 
 const totalLikes = blogs => blogs.reduce((acc, blog) => acc + parseInt(blog.likes), 0)
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => {
+    if (!max) {
+      return blog
+    }
+    if (max.likes < blog.likes) {
+      return blog
+    }
+    return max
+  }, null)
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
