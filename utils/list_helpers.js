abf4d96cb5a60c0376ee1fae49327d54f4cc80e5
letrananhvu <letrananhvu>
2020-07-03 14:18:53 +0300

    part4 4.4 - done

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = blogs => blogs.reduce((acc, blog) => acc + parseInt(blog.likes), 0)
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
